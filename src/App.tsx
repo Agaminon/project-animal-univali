691329314f29308ccdf9c742f06ed0e178207eaf
Agaminon <131570562+Agaminon@users.noreply.github.com>
2023-04-30 17:11:22 +1200

    Add files via upload
    project done

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Lobo-guará</h2>
+
+        <p>O lobo-guará (Chrysocyon brachyurus), de acordo com avaliação do Ministério do Meio Ambiente e do Instituto Chico Mendes de Conservação da Biodiversidade (ICMBio), é considerado um animal que vive em situação vulnerável para extinção</p>
+        <p>As principais ameaças ao lobo-guará são caçadores, atropelamentos e a degradação do habitat natural por atividades resultantes da ação humana.</p>
+
+      <img src="https://www.amda.org.br/images/especie_da_vez/2012.10.02_lobo_guara.jpg" alt="" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
